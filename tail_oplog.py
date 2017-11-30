05ab8e76e72335ced351d73ac41623c547acd03a
Hagai Kariti <hagai@bigpanda.io>
2017-11-30 16:41:54 +0200

    First version!
    Fetch the docs from the oplog that were added since the last sample
    (first sample starts at the script's run time). Collectd determines the
    interval. We count docs size per ns since we were started, and collectd
    outputs this data as datarate (the metric type is COUNTER).
    
    PROBLEM: collectd interval is usually 10s, while default graphite
    bucket is 60s, so only the latest sample is retained by graphite :(

---
+++ tail_oplog.py
@@ -0,0 +1,37 @@
+import time
+import pymongo
+import bson
+import collectd
+
+class MongoDBOplog(object):
+    def __init__(self):
+        self.plugin_name = "mongodb-oplog"
+        self.client = pymongo.MongoClient()
+        self.oplog = self.client.local.oplog.rs
+        self.ts = bson.timestamp.Timestamp(int(time.time()),0)
+        self.counters = dict()
+    def read(self):
+        cursor = self.oplog.find({'ts': {'$gt': self.ts}}, oplog_replay=True)
+
+        for doc in cursor:
+            self.ts = doc['ts']
+            db, collection = doc['ns'].split('.',1)
+            size = len(bson.BSON.encode(doc))
+            if not db in self.counters:
+                self.counters[db] = dict()
+            if not collection  in self.counters[db]:
+                self.counters[db][collection] = 0
+            self.counters[db][collection] += size
+
+        for db in self.counters:
+            for collection in self.counters[db]:
+                v = collectd.Values()
+                v.plugin = self.plugin_name
+                v.plugin_instance = db
+                v.type = "oplog_datarate"
+                v.type_instance = collection
+                v.values = (self.counters[db][collection],)
+                v.dispatch()
+
+mongodb_oplog = MongoDBOplog()
+collectd.register_read(mongodb_oplog.read)
